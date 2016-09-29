bcefbcc7c72585f27c37cd06561f8e1a9b8b8b33
bqxu <bcaring@163.com>
2016-09-29 23:36:58 +0800

    fix bug

--- build/build.js
+++ build/build.js
@@ -7,7 +7,7 @@ var config = require('../config')
 var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
-webpackConfig.output.publicPath='/imethod-game/static/'
+webpackConfig.output.publicPath='/imethod-game/'
 console.log(
   '  Tip:\n' +
   '  Built files are meant to be served over an HTTP server.\n' +
